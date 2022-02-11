2816e1b8d15c4e5f4af5a0b49934260900df7028
ozboware <95859352+ozboware@users.noreply.github.com>
2022-02-11 03:25:28 +0000

    Updated for NYTimes changes

--- main.js
+++ main.js
@@ -8,14 +8,14 @@ let today = month + "/" + day + "/" + year;
 let startDate = new Date("06/19/2021");
 let endDate = new Date(today);
 let days = Math.floor((endDate.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24));
-fetch ("https://www.powerlanguage.co.uk/wordle/main.e65ce0a5.js")
-	.then(x => x.text())
-	.then(y => {
-		let wordArray = y.slice(y.indexOf('var La='), y.indexOf(',Ta='))
-			.replace('var La=', '')
-			.replace('[', '')
-			.replace(']', '')
-			.replace(/"/g, '')
-			.split(',');
-		alert('The word for today (' + dayName + ' ' + day + '/' + month + '/' + year + ') is ' + wordArray[days].toUpperCase());
-	});
+fetch ("https://www.nytimes.com/games/wordle/main.4951a4aa.js")
+    .then(x => x.text())
+    .then(y => {
+        let wordArray = y.slice(y.indexOf('var Ma='), y.indexOf(',Oa='))
+            .replace('var Ma=', '')
+            .replace('[', '')
+            .replace(']', '')
+            .replace(/"/g, '')
+            .split(',');
+        alert('The word for today (' + dayName + ' ' + day + '/' + month + '/' + year + ') is ' + wordArray[days].toUpperCase());
+    });
